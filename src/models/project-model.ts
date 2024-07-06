67a660be4c342951154bd941d81242d32bb61cf0
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-07-06 18:55:37 +0800

    added a project model

---
+++ src/models/project-model.ts
@@ -0,0 +1,44 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../utils/connection";
+import User from "./user-model";
+
+const Project = sequelize.define(
+  "Project",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+export default Project;
